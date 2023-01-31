1378f3231a8521026b442efdb9846719a126770f
Sodaengdong <oisujin98@gmail.com>
2023-01-31 16:12:09 +0900

    selenium 설치 완료

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-#test
+from selenium import webdriver
+import os
+
+currentPath= os.getcwd() # getcwd() : Get + Current + Woring Directory 현재 작업 경로를 얻다
+os.chdir(currentPath)  # chdir() : 디렉토리 위치 변경
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(options=options)
+
+Google = "https://www.google.com/"
+
+driver.get(Google)
