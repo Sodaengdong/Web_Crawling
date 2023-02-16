77298bb3c0845aad220e7ecc1aead7828c6b3810
Sodaengdong <oisujin98@gmail.com>
2023-02-16 14:39:59 +0900

    백준 정답률 크롤링

--- bakjoon_chrawling.py
+++ bakjoon_chrawling.py
@@ -6,7 +6,6 @@ from bs4 import BeautifulSoup
 #웹페이지에 접근
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-
 driver = webdriver.Chrome(options=options)
 driver.get('https://www.acmicpc.net/problemset/1')
 
@@ -16,11 +15,14 @@ soup = BeautifulSoup(html,'html.parser')
 #대상 선택
 answerrate = soup.select('tbody tr td:nth-child(6)')
 sum = 0
+
 for text in answerrate:
     print(text.text[0:-1]) #float형 정답율 데이터만 추출하기 위해 '%' cut
     sum += float(text.text[0:-1]) #합계
+
 length = len(answerrate)
 acp = round(sum/(len(answerrate)),3) #평균을 소수 셋째 자리까지 표시
+
 print('Size='+str(length))
 print('Average Correct Percentage='+str(acp)+'%')
     
