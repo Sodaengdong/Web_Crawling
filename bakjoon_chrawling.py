9133b652816c95b8045f64f07be99a5c10e31955
Sodaengdong <oisujin98@gmail.com>
2023-02-16 14:47:42 +0900

    주석오타 수정

--- bakjoon_chrawling.py
+++ bakjoon_chrawling.py
@@ -16,7 +16,7 @@ answerrate = soup.select('tbody tr td:nth-child(6)')
 sum = 0
 
 for text in answerrate:
-    print(text.text[0:-1]) #float형 정답율 데이터만 추출하기 위해 '%' cut
+    print(text.text[0:-1]) #float형 정답률 데이터만 추출하기 위해 '%' cut
     sum += float(text.text[0:-1]) #합계
 
 length = len(answerrate)
