e693542e6c83a782c21276f4814c6791d9b68c03
Sodaengdong <oisujin98@gmail.com>
2023-03-09 08:36:41 +0900

    다음뉴스 1-10 페이지까지 수집

--- excel/daum_news/daum_news.py
+++ excel/daum_news/daum_news.py
@@ -10,14 +10,17 @@ search = input('검색어를 입력해주세요 : ')
 
 sheet.append(["검색어명", "기사 제목", "기사 요약"])
 
+#1~10페이지까지 수잡
 for p in range(1, 10+1, 1):
-    raw = requests.get("https://search.daum.net/search?w=news&DA=PGD&enc=utf8&cluster=y&cluster_page=1&q={s}&p={p}".format(s = search, p = p))
+    raw = requests.get("https://search.daum.net/search?nil_suggest=btn&w=news&DA=SBC&q={s}&p={p}".format(s = search, p = p))
     html = BeautifulSoup(raw.text, 'html.parser')
 
     container = html.select("div.wrap_cont")
 
     for con in container:
-        title = con.select_one(".tit_main fn_tit_u").text.strip()
+        title = con.select_one("div.wrap_cont > a").text.strip()
         content = con.select_one("p.desc").text.strip()
 
         sheet.append([search, title, content])
+
+wb.save("daum_news.xlsx")
