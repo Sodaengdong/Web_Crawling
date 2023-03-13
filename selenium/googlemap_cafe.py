574638065339136669777519d47068a7ba380533
Sodaengdong <oisujin98@gmail.com>
2023-03-13 10:24:24 +0900

    가게 컨데이터 selector하는중

--- selenium/googlemap_cafe.py
+++ selenium/googlemap_cafe.py
@@ -4,8 +4,12 @@ from selenium.webdriver.common.by import By
 import time
 
 #chrome창(웹드라이버) 열기
-service = Service("./chromedriver")
-driver = webdriver.Chrome(service=service)
+# service = Service("./chromedriver")
+# driver = webdriver.Chrome(service=service)
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(options=options)
+
 
 #실행할 웹페이지 불러도기(구글 지도)
 driver.get("https://www.google.com/maps/")
@@ -17,9 +21,6 @@ searchbutton = driver.find_element(By.CSS_SELECTOR, "#searchbox-searchbutton").c
 #999페이지까지 반복하기
 for i in range(999):
     #시간 지연
-    time.sleep(3)
-
+    time.sleep(5)
     #가게 데이터 수집
-    stores = driver.find_element(By.CSS_SELECTOR, "")
-
-
+    stores = driver.find_element(By.CSS_SELECTOR, "div.Nv2PK THOPZb CpccDe ")
